21b2c4b25aa8406037352f2feeded5a8cf4fcd96
Jonathan Leeming <jonathan@leeming.dev>
2024-01-03 18:52:41 +0000

    added metadata

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,8 +1,12 @@
-// https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   app: {
     head: {
-      title: 'KanBan'
+      title: 'KanBan',
+      charset: 'utf-8',
+      viewport: 'width=device-width, initial-scale=1',
+      meta: [
+        { name: 'description', content: 'A KanBan system' }
+      ]
     }
   },
   typescript: {
